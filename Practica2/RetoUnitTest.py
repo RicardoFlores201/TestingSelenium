a219ed77ef59b03337be24da7d87cdbfbad7313c
Ricardo Flores <richardram233@gmail.com>
2025-05-14 00:01:43 -0600

    Reducción de código. Función para iniciar ventana de la página

--- Practica2/RetoUnitTest.py
+++ Practica2/RetoUnitTest.py
@@ -5,13 +5,16 @@ from selenium.webdriver.common.by import By
 
 class RetoUnitTest(unittest.TestCase):
 
+    def initPage(self):
+        self.driver.get("https://www.saucedemo.com/v1/")
+        self.driver.maximize_window()
+
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
 
     def test_logo(self):
-        self.driver.get("https://www.saucedemo.com/v1/")
-        self.driver.maximize_window()
+        self.initPage()
         try:
             logo = self.driver.find_element(By.XPATH,"//div[@class='login_logo']")
         except NoSuchElementException:
@@ -19,8 +22,7 @@ class RetoUnitTest(unittest.TestCase):
         self.assertTrue(logo.is_displayed())
 
     def test_element_UserName(self):
-        self.driver.get("https://www.saucedemo.com/v1/")
-        self.driver.maximize_window()
+        self.initPage()
         try:
             usernamefield = self.driver.find_element(By.XPATH,"//input[@id='user-name']")
         except NoSuchElementException:
@@ -28,10 +30,17 @@ class RetoUnitTest(unittest.TestCase):
         self.assertTrue(usernamefield.is_displayed())
 
     def test_element_Password(self):
-        self.driver.get("https://www.saucedemo.com/v1/")
-        self.driver.maximize_window()
+        self.initPage()
         passwordField = self.driver.find_elements(By.XPATH, "//input[@id='password']")
         self.assertTrue(len(passwordField) > 0), "No se encontró ningún elemento"
+
+    def test_login_btn(self):
+        self.initPage()
+        try:
+            loginBtn = self.driver.find_element(By.XPATH,"//input[@id='login-button']")
+        except NoSuchElementException:
+            self.fail("No se encontró el elemento")
+        self.assertTrue(loginBtn.is_displayed())
 
     def tearDown(self):
         self.driver.quit()
